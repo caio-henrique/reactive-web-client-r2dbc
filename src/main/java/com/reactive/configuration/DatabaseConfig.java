bbffcb9888a80a96cce18fd923aaa031cf7d522c
caio-henrique <caio.silva@zup.com.br>
2019-11-28 11:28:07 -0300

    ajustes

--- src/main/java/com/reactive/configuration/DatabaseConfig.java
+++ src/main/java/com/reactive/configuration/DatabaseConfig.java
@@ -2,21 +2,12 @@ package com.reactive.configuration;
 
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
-import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.ConnectionFactoryOptions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
-import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
 import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.DatabasePopulator;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
-import org.springframework.data.r2dbc.core.DatabaseClient;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.transaction.ReactiveTransactionManager;
 
 @Configuration
@@ -33,18 +24,6 @@ class DatabaseConfig extends AbstractR2dbcConfiguration {
                         .username("teste")
                         .password("teste")
                         .build());
-
-//        ConnectionFactory connectionFactory = ConnectionFactories.get(ConnectionFactoryOptions.builder()
-//                .option(ConnectionFactoryOptions.DRIVER, "postgresql")
-//                .option(ConnectionFactoryOptions.HOST, "localhost")
-//                .option(ConnectionFactoryOptions.PORT, 5432)  // optional, defaults to 5432
-//                .option(ConnectionFactoryOptions.USER, "teste")
-//                .option(ConnectionFactoryOptions.PASSWORD, "teste")
-//                .option(ConnectionFactoryOptions.DATABASE, "reactive")  // optional
-////                .option(OPTIONS, options) // optional
-//                .build());
-//
-//        return connectionFactory;
     }
 
     @Bean
@@ -65,4 +44,5 @@ class DatabaseConfig extends AbstractR2dbcConfiguration {
 
         return initializer;
     }
+
 }
--- src/main/java/com/reactive/controller/PersonController.java
+++ src/main/java/com/reactive/controller/PersonController.java
@@ -5,16 +5,11 @@ import com.reactive.model.Person;
 import com.reactive.repository.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/person")
 public class PersonController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class);
@@ -25,19 +20,15 @@ public class PersonController {
         this.personRepository = personRepository;
     }
 
-    @GetMapping("/json")
-//    @Transactional
-    public Flux<Person> findPersonsJson() {
+    @GetMapping
+    public Flux<Person> findPersons() {
 
         Flux<Person> persons = personRepository.findAll();
-
-//        persons.map(person -> person.setFirstName(person.getFirstName().toLowerCase()));
-
-//        persons.flatMap(person -> new Person(person.getId(), person.getFirstName().toLowerCase(), person.getLastName().toLowerCase()));
-
-        persons.delayElements(Duration.ofMillis(1000))
-                .subscribe(person -> LOGGER.info("Client subscribes: {}", person));
-
         return persons;
+    }
+
+    @PostMapping
+    public void cretePerson(@RequestBody Person person){
+         personRepository.save(person).subscribe();
     }
 }
